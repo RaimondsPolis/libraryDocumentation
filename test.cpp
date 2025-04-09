4837cedde48e9ebd9bba26b384f3741cb86495f4
RaimondsPolis <149487123+RaimondsPolis@users.noreply.github.com>
2025-04-09 15:00:07 +0300

    ASDSERGDFTH

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // Your code starts here
+    cout << "Hello, World!" << endl;
+
+    return 0; // Indicate successful program termination
+}
